e73307d3714b485b550e8885a23c3d61a99c4a7c
nnareshmgg@gmail.com <Pink2428$>
2017-02-02 09:23:28 -0800

    added bigquery to check if it works on heroku

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
+from google.cloud import bigquery
+
+
 # Flask app should start in global layout
 app = Flask(__name__)
 
